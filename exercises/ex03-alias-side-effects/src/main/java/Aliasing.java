6d32da7d36a28c66bcb2a98fdb988bb7960f81a8
ascodes17 <ava.chyung@mail.utoronto.ca>
2026-09-24 01:40:41 -0400

    Implemented exercise 3

--- exercises/ex03-alias-side-effects/src/main/java/Aliasing.java
+++ exercises/ex03-alias-side-effects/src/main/java/Aliasing.java
@@ -40,6 +40,9 @@ public class Aliasing {
      */
     public static void addInPlace(int[] arr, int amount) {
         // TODO: complete
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += amount;
+        }
     }
 
     /**
@@ -52,6 +55,12 @@ public class Aliasing {
      */
     public static int[] addCopy(int[] arr, int amount) {
         // TODO: complete
-        return new int[1];
+        int[] elements = new int[arr.length];
+        int i = 0;
+        for (int s : arr) {
+            elements[i] = s + amount;
+            i++;
+        }
+        return elements;
     }
 }
